aec0ca4ead9404e0fc19bec185fbd9d1f94bb25a
HakanK003 <hkaratashk3@gmail.com>
2023-01-09 18:07:56 -0500

    First 37 Task done

---
+++ JavaScriptAlgorithmsAndDataStructuresCourse/BasicJavaScript.js
@@ -0,0 +1,240 @@
+{
+// 001 - Comment Your JavaScript Code
+    // This is an in-line comment.
+    /* 
+    This is a
+    multi-line comment
+    */
+}
+
+{
+// 002 - Declare JavaScript Variables
+    var myName;
+}
+
+{
+// 003 - Storing Values with the Assignment Operator
+    // Setup
+    var a;
+    // Only change code below this line
+    a = 5;
+}
+
+{
+// 004 - Assigning the Value of One Variable to Another
+    // Setup
+    var a;
+    a = 7;
+    var b;
+    // Only change code below this line
+    b = a;
+}
+
+{
+// 005 - Initializing Variables with the Assignment Operator
+    var a = 9;
+}
+
+{
+// 006 - Declare String Variables
+    var myFirstName = 'H';
+    var myLastName = 'K';
+}
+
+{
+// 007 - Understanding Uninitialized Variable
+    // Only change code below this line
+    var a = 5;
+    var b = 10;
+    var c = "I am a";
+    // Only change code above this line
+    a = a + 1;
+    b = b + 5;
+    c = c + " String!";
+}
+
+{
+// 008 - Understanding Case Sensitivity in Variables
+    // Variable declarations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+    // Variable assignments
+    studlyCapVar = 10;
+    properCamelCase  = "A String";
+    titleCaseOver = 9000;
+}
+
+{
+// 009 - Explore Differences Between the var and let Keywords
+    let catName = "Oliver";
+    let catSound = "Meow!";
+}
+
+{
+// 010 - Declare a Read-Only Variable with the const Keyword
+    const FCC = "freeCodeCamp"; // Change this line
+    let fact = "is cool!"; // Change this line
+    fact = "is awesome!";
+    console.log(FCC, fact); // Change this line
+}
+
+{
+// 011 - Add Two Numbers with JavaScript
+    const sum = 10 + 10;
+}
+
+{
+// 012 - Subtract One Number from Another with JavaScript
+    const difference = 45 - 33;
+}
+
+{
+// 013 - Multiply Two Numbers with JavaScript
+    const product = 8 * 10;
+}
+
+{
+// 014 - Divide One Number by Another with JavaScript
+    const quotient = 66 / 33;
+}
+
+{
+// 015 - Increment a Number with JavaScript
+    let myVar = 87;
+    // Only change code below this line
+    myVar++;
+}
+
+{
+// 016 - Decrement a Number with JavaScript
+    let myVar = 11;
+    // Only change code below this line
+    myVar--;
+}
+
+{
+// 017 - Create Decimal Numbers with JavaScript
+    const ourDecimal = 5.7;
+    // Only change code below this line
+    const myDecimal = 5.7;
+}
+
+{
+// 018 - Multiply Two Decimals with JavaScript
+    const product = 2.0 * 2.5;
+}
+
+{
+// 019 - Divide One Decimal by Another with JavaScript
+    const quotient = 4.4 / 2.0; // Change this line
+}
+
+{
+// 020 - Finding a Remainder in JavaScript
+    const remainder = 11 % 3;
+}
+
+{
+// 021 - Compound Assignment With Augmented Addition
+    let a = 3;
+    let b = 17;
+    let c = 12;
+    // Only change code below this line
+    a += 12;
+    b += 9;
+    c += 7;
+}
+
+{
+// 022 - Compound Assignment With Augmented Subtraction
+    let a = 11;
+    let b = 9;
+    let c = 3;
+    // Only change code below this line
+    a -= 6;
+    b -= 15;
+    c -= 1;
+}
+
+{
+// 023 - Compound Assignment With Augmented Multiplication
+    let a = 5;
+    let b = 12;
+    let c = 4.6;
+    // Only change code below this line
+    a *= 5;
+    b *= 3;
+    c *= 10;
+}
+
+{
+// 024 - Compound Assignment With Augmented Division
+    let a = 48;
+    let b = 108;
+    let c = 33;
+    // Only change code below this line
+    a /= 12;
+    b /= 4;
+    c /= 11;
+}
+
+{
+// 025 - Escaping Literal Quotes in Strings
+    const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+}
+
+{
+// 026 - Quoting Strings with Single Quotes
+    const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+}
+
+{
+// 027 - Escape Sequences in Strings
+    const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+}
+
+{
+// 028 - Concatenating Strings with Plus Operator
+    const myStr = "This is the start. " + "This is the end."; // Change this line
+}
+
+{
+// 029 - Concatenating Strings with the Plus Equals Operator
+    let myStr = "This is the first sentence. ";
+    myStr+="This is the second sentence.";
+}
+
+{
+// 030 - Constructing Strings with Variables
+    // Only change code below this line
+    const myName = "myName";
+    const myStr = "My name is " + myName + " and I am well!";
+}
+
+
+{
+// 031 - Appending Variables to Strings
+    // Change code below this line
+    const someAdjective = "cool";
+    let myStr = "Learning to code is ";
+    myStr+=someAdjective;
+}
+
+{
+// 032 - Find the Length of a String
+    // Setup
+    let lastNameLength = 0;
+    const lastName = "Lovelace";
+    // Only change code below this line
+    lastNameLength = lastName.length;
+}
+
+{
+// 033 - Find the Length of a String
+    // Setup
+    let firstLetterOfLastName = "";
+    const lastName = "Lovelace";
+    // Only change code below this line
+    firstLetterOfLastName = lastName[0]; // Change this line
+}
